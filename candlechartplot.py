aef1f5f45fe2c20de837bea33c835576c1b71c85
David Marques <david@david-t420>
2019-12-08 17:57:49 +0000

    Updated Readme

--- candlechartplot.py
+++ candlechartplot.py
@@ -6,15 +6,13 @@ import pandas as pd
 def plot_candle_chart(path_to_csv: str, date_format="%Y-%m-%d") -> None:
     """
     Simple function to plot a candle chart.
-    This assumes that the file has 4 columns:
+    This assumes that the files header has 4 columns:
 
     - Date: The date in the specified format
     - Open: The open price
     - Close: The Close price
     - High: The highest price
     - Low: The lowest price
-
-    The exact names of the columns do not matter, that the csv needs to have a header with the columns in this order.
 
     :param path_to_csv: Path to the csv file
     :param date_format: The format of the Date column
