cbc87e3c40063925bd6a4c298ee8667e2f976832
David Marques <david@david-t420>
2019-12-08 12:10:35 +0000

    First commit

---
+++ candlechartplot.py
@@ -0,0 +1,10 @@
+import matplotlin.pyplot as plt
+import pandas as pd
+
+
+def plot_candle_chart(path_to_csv: str) -> None:
+    """
+    Simple function to plot a candle chart.
+    This assumes that
+
+    """
